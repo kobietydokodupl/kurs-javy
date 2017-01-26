744faefa900027ea9e4dbba99d5d724549a48dc9
pigularz <mafrapa@gmail.com>
2017-01-26 15:39:39 +0100

    New instance of stage

--- src/main/java/pl/kobietydokodu/koty/Interfejs.java
+++ src/main/java/pl/kobietydokodu/koty/Interfejs.java
@@ -3,6 +3,7 @@ package pl.kobietydokodu.koty;
 import java.util.Scanner;
 
 import pl.kobietydokodu.koty.domain.Kot;
+import pl.kobietydokodu.koty.domain.Pies;
 
 
 /**
@@ -21,16 +22,27 @@ public class Interfejs {
      */
     public static void main(String[] args) {
         Kot kot = new Kot();
+        Pies pies = new Pies();
 
         System.out.print("Podaj imię kota: ");
         kot.setImie(getUserInput());
 
+        System.out.print("Podaj imię psa: ");
+        pies.setDogName(getUserInput());
+        
         System.out.print("Podaj, kto jest opiekunem kota: ");
         kot.setImieOpiekuna(getUserInput());
 
+        System.out.print("Podaj, kto jest opiekunem psa: ");
+        pies.setDogOwner(getUserInput());
+        
+        
+        
         System.out.println("Dziękuję, teraz już wiem prawie wszystko o kocie!");
     
         System.out.println("A więc kot to " + kot.getImie() + " , a opiekun to: " + kot.getImieOpiekuna());
+    
+        System.out.println("Twierdzisz, że pies, to: " + pies.getDogName() + " ? \nNo a jego opiekun, to: " + pies.getDogOwner());
     }
     
 
