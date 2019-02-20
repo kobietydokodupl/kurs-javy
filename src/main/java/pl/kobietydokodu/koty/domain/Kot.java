cd9445bea5eeb39de939b9966a0c39db4d94b983
ines <i.bouhajeb@gmail.com>
2019-02-20 19:33:20 +0100

    Orginal code modified for my version

--- src/main/java/pl/kobietydokodu/koty/domain/Kot.java
+++ src/main/java/pl/kobietydokodu/koty/domain/Kot.java
@@ -1,70 +1,51 @@
 package pl.kobietydokodu.koty.domain;
 
 import java.util.Date;
-
-/**
- * Klasa reprezentująca kota.
- */
 public class Kot {
-
-    /**
-     * Imię kota.
-     */
-    private String imie;
-    
-    /**
-     * Data urodzenia kota.
-     */
-    private Date dataUrodzenia;
-    
-    /**
-     * Waga kota.
-     */
-    private Float waga;
-    
-    /**
-     * Imię opiekuna kota.
-     */
-    private String imieOpiekuna;
 	
-    /**
-     * Metoda, która opisuje kota pełnym zdaniem.
-     * @return Opis kota w postaci zdania.
-     */
+	private String imie;
+	private Date data;
+	private Float waga;
+	private String imieOpiekuna;
+	
 	public String przedstawSie() {
-		return "Cześć, jestem "+imie+", urodziłem się "+dataUrodzenia+", ważę "+waga+", a opiekuje się mną " + imieOpiekuna;
+		return this.imie+" "+this.data+" "+this.waga+" "+this.imieOpiekuna;
+	}
+	
+	
+	public String getImie() {
+		return imie;
+	}
+	public void setImie(String imie) {
+		this.imie = imie;
+	}
+	public Date getData() {
+		return data;
+	}
+	public void setData(Date data) {
+		this.data = data;
+	}
+	public Float getWaga() {
+		return waga;
+	}
+	public void setWaga(Float waga) {
+		this.waga = waga;
+	}
+	public String getImieOpiekuna() {
+		return imieOpiekuna;
+	}
+	public void setImieOpiekuna(String imieOpiekuna) {
+		this.imieOpiekuna = imieOpiekuna;
 	}
 
-    public String getImie() {
-        return imie;
-    }
 
-    public void setImie(String imie) {
-        this.imie = imie;
-    }
-
-    public Date getDataUrodzenia() {
-        return dataUrodzenia;
-    }
-
-    public void setDataUrodzenia(Date dataUrodzenia) {
-        this.dataUrodzenia = dataUrodzenia;
-    }
-
-    public Float getWaga() {
-        return waga;
-    }
-
-    public void setWaga(Float waga) {
-        this.waga = waga;
-    }
-
-    public String getImieOpiekuna() {
-        return imieOpiekuna;
-    }
-
-    public void setImieOpiekuna(String imieOpiekuna) {
-        this.imieOpiekuna = imieOpiekuna;
-    }
+	@Override
+	public String toString() {
+		return "Kot [imie=" + imie + ", data=" + data + ", waga=" + waga + ", imieOpiekuna=" + imieOpiekuna + "]";
+	}
+	
+	
+	
+	
 
 }
